66b11fbc5b3bc02c6a6cd9ce721dbb680390597a
yoyobara <yotambarak6@gmail.com>
2021-04-21 18:14:42 +0300

    added gitIgnore

--- js/qeMath.js
+++ js/qeMath.js
@@ -4,7 +4,7 @@ const pow = Math.pow;
 
 var solveQe = (a, b, c) => {
     let res = new Array(2);
-    let ic = 0;
+    let ic = 0; // indexCounter
 
     let root = pow(b, 2) - 4 * a * c;
     if (root < 0) return res.fill(NaN);
